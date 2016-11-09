3643664b53ca6df48f1a7f2856186d4c3462c9a7
Pawan Kumar <pawan221b@gmail.com>
2016-11-09 09:56:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pawankumar/webapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.pawankumar.webapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+
+    @Override
+    public void onBackPressed() {
+        if(webview.canGoBack()){
+            webview.goBack();
+        }else {
+
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webview.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webview.setWebViewClient(new WebViewClient());
+
+        webview.loadUrl("https://www.google.com");
+
+    }
+}
